453767cf491c8f5a9826d129913db32996130769
Matt Dias <mld78@case.edu>
2017-04-18 12:45:27 -0700

    added email for contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,19 +8,19 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'me@myprogrammingblog.com'; // put your email
+
+// create email body and send it
+$to = 'mld78@case.edu'; // put your email
 $email_subject = "Contact form submitted by:  $name";
 $email_body = "You have received a new message. \n\n".
 				  " Here are the details:\n \nName: $name \n ".
 				  "Email: $email_address\n Message \n $message";
 $headers = "From: contacts@myprogrammingblog.com\n";
-$headers .= "Reply-To: $email_address";	
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
